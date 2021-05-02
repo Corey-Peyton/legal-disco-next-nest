952e4d9d364cd5936874ffa8ed38412c60753e83
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-02 19:42:35 +0530

    so built in routing still works. we should use that only. because in future if we want to have seperate vue app, this will be easier.

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,3 +1,13 @@
 <template>
-  <Nuxt />
+  <vertical-container id="app">
+    <nav-bar v-if="['annotator'].indexOf($route.name) === -1"></nav-bar>
+    <router-view class="flex-remaining flex-column fullHeight" />
+  </vertical-container>
 </template>
+
+<script lang="ts" src="./default.ts"/>
+
+<style lang="sass">
+$material-icons-font-path: '~material-icons/iconfont/'
+@import '~material-icons/iconfont/material-icons.scss'
+</style>
